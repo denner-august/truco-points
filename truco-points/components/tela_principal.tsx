379d80fe08c3c94ff2829f8925996d24931e1805
denner augusto <denner.augusto90@gmail.com>
2023-01-31 19:46:16 -0300

    Create tela_principal.tsx
    transferencia da tela principal outro component

---
+++ truco-points/components/tela_principal.tsx
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Nomeando } from '../components/nomeando';
+
+export function TelaPrincipal({ navigation }) {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.ContainerTitulo}>
+                <Text style={styles.titulo}>Nome dos Jogadores</Text>
+            </View>
+            <Nomeando />
+            <Nomeando />
+
+            <TouchableOpacity onPress={() => navigation.navigate("Game")}>
+                <Text style={styles.ButtonTitulo}>Continuar</Text>
+            </TouchableOpacity>
+            <StatusBar style="light" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 55,
+        flex: 1,
+        backgroundColor: '#181313',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    ContainerTitulo: {
+
+    },
+    titulo: {
+        color: "#ffff",
+        fontSize: 25
+    },
+    ButtonTitulo: {
+        color: "#ffff",
+        fontSize: 25
+    }
+});
