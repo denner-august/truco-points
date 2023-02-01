0a5ea69dcee06c197658664528e731127937372b
denner augusto <denner.augusto90@gmail.com>
2023-02-01 18:58:30 -0300

    Update nomeando.tsx
    A mudança feita agora gerencia melhor os jogadores e adiciona tipagem ao id

--- truco-points/components/nomeando.tsx
+++ truco-points/components/nomeando.tsx
@@ -1,19 +1,35 @@
-import { useState } from "react";
-import { View, StyleSheet, TextInput } from "react-native";
+import { useContext } from "react";
+import { View, StyleSheet, TextInput, Text } from "react-native";
 
-export function Nomeando() {
+import { Context } from "../context/stateManager";
 
-    const [JogadorA, setJogadorA] = useState("")
-    const [jogadorB, setJogadorB] = useState("")
+interface NomeandoProps {
+    id: 1 | 2
+}
+
+export function Nomeando({ id }: NomeandoProps) {
+
+    const { JogadorA, jogadorB, setJogadorA, setJogadorB } = useContext(Context)
+    const { JogadorC, jogadorD, setJogadorC, setJogadorD } = useContext(Context)
+
+    const verificaDuplas = {
+        duplaA: id === 1 ? JogadorA : JogadorC,
+        duplaB: id === 2 ? jogadorB : jogadorD
+    }
+
+    const MudaNomeVerificaDupla = {
+        duplaA: id === 1 ? setJogadorA : setJogadorC,
+        duplaB: id === 2 ? setJogadorB : setJogadorD
+    }
 
     return (
         <View style={styles.Container}>
             <View>
-                <TextInput value={JogadorA} onChangeText={(value) => setJogadorA(value)} style={styles.input} placeholder="Jogador-A" placeholderTextColor="#8d8c8c" />
+                <TextInput value={verificaDuplas.duplaA} onChangeText={(value) => MudaNomeVerificaDupla.duplaA(value)} style={styles.input} placeholder="Jogador-A" placeholderTextColor="#8d8c8c" />
             </View>
 
             <View>
-                <TextInput value={jogadorB} onChangeText={(value) => setJogadorB(value)} style={styles.input}
+                <TextInput value={verificaDuplas.duplaB} onChangeText={(value) => MudaNomeVerificaDupla.duplaB(value)} style={styles.input}
                     placeholder="Jogador-B" placeholderTextColor="#8d8c8c" />
             </View>
         </View>
