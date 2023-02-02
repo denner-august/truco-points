663d78df52411c22f04305bc2783a8ba10cdda51
denner augusto <denner.augusto90@gmail.com>
2023-02-02 15:55:13 -0300

    Create telaPrincipal.test.tsx

---
+++ truco-points/__test__/view/telaPrincipal.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer, { create } from 'react-test-renderer';
+
+import { Nomeando } from '../../components/nomeando'
+
+const component = create(<Nomeando />)
+
+describe('<Nomeando />', () => {
+    it('procura quantidade de inputs', () => {
+        const findInputA = component.root.findByProps({ testID: "Jogador-A" }).props;
+        expect(findInputA.placeholder).toEqual("Jogador-A")
+
+        const findInputB = component.root.findByProps({ testID: "Jogador-B" }).props;
+        expect(findInputB.placeholder).toEqual("Jogador-B")
+    })
+});
