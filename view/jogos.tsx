1523129d25a43f069cbadaaecfc6702e98b7fc9f
denner augusto <denner.augusto90@gmail.com>
2023-02-03 13:05:03 -0300

    test(testes): adicinando novos testes a tela que de jogos que será desenvolvida

--- view/jogos.tsx
+++ view/jogos.tsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { Context } from "../context/stateManager";
 
 export function Jogos() {
+
+    const { JogadorA, jogadorB } = useContext(Context)
+    const { JogadorC, jogadorD } = useContext(Context)
+
     return (
         <View style={styles.Container}>
             <Text style={{ color: "#ffff" }}>tela dos jogos</Text>
@@ -16,6 +22,4 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: 'center'
     }
-
-
 })
