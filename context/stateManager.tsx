1140e1ac017000f0ffe8f5d0442bdf85967bbd09
denner augusto <denner.augusto90@gmail.com>
2023-02-06 18:01:48 -0300

    refactor(project): adicionando placares ao state global

--- context/stateManager.tsx
+++ context/stateManager.tsx
@@ -5,14 +5,25 @@ export const Context = createContext({} as ContextProps)
 
 export function ContextProvider({ children }: childrenProps) {
 
-    const [JogadorA, setJogadorA] = useState("")
-    const [jogadorB, setJogadorB] = useState("")
+    const [JogadorA, setJogadorA] = useState("Denner")
+    const [jogadorB, setJogadorB] = useState("marco")
 
-    const [JogadorC, setJogadorC] = useState("")
-    const [jogadorD, setJogadorD] = useState("")
+    const [JogadorC, setJogadorC] = useState("marisa")
+    const [jogadorD, setJogadorD] = useState("mayara")
+
+    const [placarTimeA, setPlacarTimeA] = useState(0)
+    const [placarTimeB, setPlacarTimeB] = useState(0)
+
+    function verifyPlayers() {
+        if (JogadorA !== "" && jogadorB !== "" && JogadorC !== "" && jogadorD !== "") {
+            return true
+        }
+
+        false
+    }
 
     return (
-        <Context.Provider value={{ JogadorA, setJogadorA, jogadorB, setJogadorB, JogadorC, setJogadorC, jogadorD, setJogadorD }}>
+        <Context.Provider value={{ JogadorA, setJogadorA, jogadorB, setJogadorB, JogadorC, setJogadorC, jogadorD, setJogadorD, placarTimeA, setPlacarTimeA, placarTimeB, setPlacarTimeB, verifyPlayers }}>
             {children}
         </Context.Provider>
     )
