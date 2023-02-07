3e4653f11fa9da315a0a07af407d6e74cce1b7ac
denner augusto <denner.augusto90@gmail.com>
2023-02-07 13:48:23 -0300

    refactor(project): adicionando teste verificando quantidade de buttons na tela

--- __test__/view/telasJogos.test.tsx
+++ __test__/view/telasJogos.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer, { create } from 'react-test-renderer';
 
 import { Jogos } from '../../view/jogos'
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Component = create(<Jogos />)
 
@@ -18,6 +18,11 @@ describe("teste de funcionalidade dos jogos", () => {
 
     test('testando se os 4 jogadores estão na tela', () => {
         const rootComponent = Component.root.findAllByType(Text)
-        expect(rootComponent.length).toBe(7)
+        expect(rootComponent.length).toBe(9)
+    })
+
+    test('testando se os buttons estão  estão na tela', () => {
+        const rootComponent = Component.root.findAllByType(TouchableOpacity)
+        expect(rootComponent.length).toBe(6)
     })
 })
