b2334b38a303732c2dcb2a272bff306bf6ce356d
denner augusto <denner.augusto90@gmail.com>
2023-02-07 13:47:16 -0300

    refactor(project): refatorando component de jogos, agora adicionando placar e botão auxiliar

--- view/jogos.tsx
+++ view/jogos.tsx
@@ -1,20 +1,47 @@
 import { useContext } from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { Context } from "../context/stateManager";
+
+import { tamanhoTela } from "../functions/dimensions";
 
 export function Jogos() {
 
     const { JogadorA, jogadorB, placarTimeA, setPlacarTimeA } = useContext(Context)
     const { JogadorC, jogadorD, placarTimeB, setPlacarTimeB } = useContext(Context)
 
+    function diminui(state: number, funcao: () => void) {
+        if (state <= 0) {
+            return
+        }
+        funcao()
+    }
+
+    function aumentar(state: number, funcao: () => void, jogar1: string, jogador2) {
+        if (state + 1 === 12) {
+            funcao()
+            return Alert.alert(`${jogar1} e ${jogador2} ganharam`)
+        }
+        if (state >= 12) {
+            return
+        }
+        funcao()
+    }
+
     return (
         <View style={styles.Container}>
             <View style={styles.ViewChildrens}>
-                <Text style={styles.nomes}>{JogadorA}</Text>
-                <Text style={styles.nomes}> {jogadorD}</Text>
+                <Text style={styles.nomes}>{JogadorA} {jogadorD}</Text>
 
-                <TouchableOpacity onPress={() => setPlacarTimeA(placarTimeA + 1)}>
+                <TouchableOpacity style={styles.button} disabled>
                     <Text style={styles.nomes} >{placarTimeA}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button} onPress={() => aumentar(placarTimeA, () => setPlacarTimeA(placarTimeA + 1), JogadorA, jogadorD)}>
+                    <Text style={styles.nomes} >Aumenta</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.diminui} onPress={() => diminui(placarTimeA, () => setPlacarTimeA(placarTimeA - 1))}>
+                    <Text style={styles.nomes} >Diminui</Text>
                 </TouchableOpacity>
             </View>
 
@@ -23,11 +50,19 @@ export function Jogos() {
 
 
             <View style={styles.ViewChildrens}>
-                <Text style={styles.nomes}>{JogadorC}</Text>
-                <Text style={styles.nomes}>{jogadorB}</Text>
+                <Text style={styles.nomes}> {jogadorB} {JogadorC}</Text>
 
-                <TouchableOpacity onPress={() => setPlacarTimeB(placarTimeB + 1)}>
+
+                <TouchableOpacity style={styles.button} disabled>
                     <Text style={styles.nomes}>{placarTimeB}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button} onPress={() => aumentar(placarTimeB, () => setPlacarTimeB(placarTimeB + 1), jogadorB, JogadorC)}>
+                    <Text style={styles.nomes} >Aumenta</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.diminui} onPress={() => diminui(placarTimeB, () => setPlacarTimeB(placarTimeB - 1))}>
+                    <Text style={styles.nomes}>Diminui</Text>
                 </TouchableOpacity>
             </View>
         </View >
@@ -43,12 +78,33 @@ const styles = StyleSheet.create({
     },
 
     ViewChildrens: {
-        alignItems: "center"
+        flex: 1,
+        width: tamanhoTela.largura,
+        alignItems: "center",
+        justifyContent: "center"
     },
 
     nomes: {
         fontSize: 25,
-        color: "#ffff"
+        color: "#ffff",
+        textAlign: "center"
+    },
 
+    button: {
+        borderWidth: 1,
+        borderColor: "#ffff",
+        padding: 5,
+        width: (80 * tamanhoTela.largura) / 100,
+        borderRadius: 10,
+        marginVertical: 5
+    },
+
+    diminui: {
+        borderWidth: 1,
+        borderColor: "#ffff",
+        padding: 5,
+        width: (80 * tamanhoTela.largura) / 100,
+        borderRadius: 10,
+        marginVertical: 5
     }
 })
